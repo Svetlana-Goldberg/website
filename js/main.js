f197958e3eebd0846151f9a1ca1969e34cfbdf8b
Svetlana-Goldberg <svetlanagoldberg9@gmail.com>
2024-07-17 20:04:14 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,45 @@
+// $(document).on('click', '.main--drop-down', function (e) {
+//     let elements = document.getElementsByClassName("main--drop-down");
+//     let isActive = $(this).hasClass("active");
+
+//     if (isActive) {
+//         $(this).children(".main--drop-down-info").slideUp();
+//         $(this).removeClass("active");
+//     } else {
+//         for (let i = 0; i < elements.length; i++) {
+//             elements[i].classList.remove("active");
+//         }
+
+//         $(elements).children(".main--drop-down-info").slideUp();
+//         $(this).children(".main--drop-down-info").slideDown();
+//         $(this).addClass("active");
+//     }
+//     e.stopPropagation(); // Prevent the click event from bubbling up to document
+// });
+
+// $(document).mouseup(function (e) {
+//     let container = $(".main");
+//     if (container.has(e.target).length === 0) {
+//         let elements = document.getElementsByClassName("main--drop-down");
+//         let dropDowns = document.getElementsByClassName("main--drop-down-info");
+//         if ($(elements).hasClass("active")) {
+//             $(elements).removeClass("active");
+//             $(dropDowns).slideUp();
+//         }
+//     }
+// });
+
 $(document).on('click', '.main--drop-down', function (e) {
+    console.log('Clicked on drop-down');
     let elements = document.getElementsByClassName("main--drop-down");
     let isActive = $(this).hasClass("active");
 
     if (isActive) {
+        console.log('Closing drop-down');
         $(this).children(".main--drop-down-info").slideUp();
         $(this).removeClass("active");
     } else {
+        console.log('Opening drop-down');
         for (let i = 0; i < elements.length; i++) {
             elements[i].classList.remove("active");
         }
@@ -18,8 +52,10 @@ $(document).on('click', '.main--drop-down', function (e) {
 });
 
 $(document).mouseup(function (e) {
+    console.log('Mouse up on document');
     let container = $(".main");
     if (container.has(e.target).length === 0) {
+        console.log('Clicked outside container');
         let elements = document.getElementsByClassName("main--drop-down");
         let dropDowns = document.getElementsByClassName("main--drop-down-info");
         if ($(elements).hasClass("active")) {

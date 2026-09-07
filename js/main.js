8a52298e8c3e7746d8fb003c0f66e16bca5266c4
Svetlana-Goldberg <svetlanagoldberg9@gmail.com>
2026-09-08 02:46:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,47 @@
-$(document).on('click', '.main--drop-down', function (e) {
-    let elements = document.getElementsByClassName("main--drop-down");
-    let isActive = $(this).hasClass("active");
+// $(document).on('click', '.main--drop-down', function (e) {
+//     let elements = document.getElementsByClassName("main--drop-down");
+//     let isActive = $(this).hasClass("active");
 
-    if (isActive) {
-        $(this).children(".main--drop-down-info").slideUp();
-        $(this).removeClass("active");
+//     if (isActive) {
+//         $(this).children(".main--drop-down-info").slideUp();
+//         $(this).removeClass("active");
+//     } else {
+//         for (let i = 0; i < elements.length; i++) {
+//             elements[i].classList.remove("active");
+//         }
+
+//         $(elements).children(".main--drop-down-info").slideUp();
+//         $(this).children(".main--drop-down-info").slideDown();
+//         $(this).addClass("active");
+//     }
+//     e.stopPropagation(); // Prevent the click event from bubbling up to document
+// });
+
+// $(document).mouseup(function (e) {
+//     let container = $(".main");
+//     if (container.has(e.target).length === 0) {
+//         let elements = document.getElementsByClassName("main--drop-down");
+//         let dropDowns = document.getElementsByClassName("main--drop-down-info");
+
+//         let activeElement = document.querySelector(".main--drop-down.active");
+//         if (activeElement && !activeElement.contains(e.target)) {
+//             $(elements).removeClass("active");
+//             $(dropDowns).slideUp();
+//         }
+//     }
+// });
+
+$(document).on('click', '.main--drop-down-top', function (e) {
+    e.stopPropagation();
+
+    const currentDropDown = $(this).parent('.main--drop-down');
+    const currentInfo = currentDropDown.children('.main--drop-down-info');
+
+    if (currentDropDown.hasClass('active')) {
+        currentInfo.slideUp();
+        currentDropDown.removeClass('active');
     } else {
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.remove("active");
-        }
-
-        $(elements).children(".main--drop-down-info").slideUp();
-        $(this).children(".main--drop-down-info").slideDown();
-        $(this).addClass("active");
-    }
-    e.stopPropagation(); // Prevent the click event from bubbling up to document
-});
-
-$(document).mouseup(function (e) {
-    let container = $(".main");
-    if (container.has(e.target).length === 0) {
-        let elements = document.getElementsByClassName("main--drop-down");
-        let dropDowns = document.getElementsByClassName("main--drop-down-info");
-
-        let activeElement = document.querySelector(".main--drop-down.active");
-        if (activeElement && !activeElement.contains(e.target)) {
-            $(elements).removeClass("active");
-            $(dropDowns).slideUp();
-        }
+        currentInfo.slideDown();
+        currentDropDown.addClass('active');
     }
 });
